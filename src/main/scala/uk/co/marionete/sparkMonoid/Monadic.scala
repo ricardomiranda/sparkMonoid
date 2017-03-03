a7f7fe437790db3b83ed7b07c32595bf780d3a6c
Ricardo Miranda <mail@ricardoMiranda.com>
2017-03-03 12:31:48 +0000

    monadic function

--- src/main/scala/uk/co/marionete/sparkMonoid/Monadic.scala
+++ src/main/scala/uk/co/marionete/sparkMonoid/Monadic.scala
@@ -3,20 +3,17 @@ package uk.co.marionete.sparkMonoid
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.rdd._
 
-case class Monadic(charactersRDD: RDD[Char],
+case class Monadic(charactersRDD: RDD[Seq[Char]],
 	           spark: SparkSession) {
   def agg: String = {
-    ???
+    this.charactersRDD.reduce{ (x, y) => x ++ y }.mkString
   }
 }
 
 object Monadic {
   def apply(cs: Seq[Char], 
 	    spark: SparkSession) = {
-    val size = 1
-    val charactersRDD =
-      spark.sparkContext.parallelize(cs)
-
+    val charactersRDD = spark.sparkContext.parallelize(cs.map(Seq(_)))
     new Monadic(charactersRDD, spark)
   }
 }
