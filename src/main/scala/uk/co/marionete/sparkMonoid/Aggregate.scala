3dad25a3b8cf2b34dc84412359b34583544ee946
Ricardo Miranda <mail@ricardoMiranda.com>
2017-03-03 14:43:41 +0000

    this

--- src/main/scala/uk/co/marionete/sparkMonoid/Aggregate.scala
+++ src/main/scala/uk/co/marionete/sparkMonoid/Aggregate.scala
@@ -6,9 +6,9 @@ import org.apache.spark.rdd._
 case class Aggregate(charactersRDD: RDD[Char],
 	             spark: SparkSession) {
   def agg: String = {
-    charactersRDD.aggregate("") (
-	    (acc, c) => acc + String.valueOf(c),
-	    (acc1, acc2) => acc1 + acc2
+    this.charactersRDD.aggregate("") (
+      (acc, c) => acc + String.valueOf(c),
+      (acc1, acc2) => acc1 + acc2
     )
   }
 }
